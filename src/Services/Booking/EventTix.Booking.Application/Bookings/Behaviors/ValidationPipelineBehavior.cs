f346b531ca1e1aed6c1a536fec2040a753b1d5ba
Andrea Parisi <andreaparisi917@gmail.com>
2026-07-30 16:28:23 +0200

    feat(booking): add validation pipeline

---
+++ src/Services/Booking/EventTix.Booking.Application/Bookings/Behaviors/ValidationPipelineBehavior.cs
@@ -0,0 +1,48 @@
+﻿using FluentValidation;
+using MediatR;
+
+namespace EventTix.Booking.Application.Bookings.Behaviors
+{
+    public sealed class ValidationPipelineBehavior<TRequest, TResponse>
+        : IPipelineBehavior<TRequest, TResponse> where TRequest : notnull 
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationPipelineBehavior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(
+            TRequest request,
+            RequestHandlerDelegate<TResponse> next,
+            CancellationToken cancellationToken)
+        {
+            if (!_validators.Any())
+            {
+                #pragma warning disable CA2016
+                return await next();
+                #pragma warning restore CA2016
+            }
+
+            var context = new ValidationContext<TRequest>(request);
+
+            var validationResults = await Task.WhenAll(
+                _validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+
+            var failures = validationResults
+                .SelectMany(r => r.Errors)
+                .Where(f => f != null)
+                .ToList();
+
+            if (failures.Count != 0)
+            {
+                throw new ValidationException(failures);
+            }
+
+            #pragma warning disable CA2016
+            return await next();
+            #pragma warning restore CA2016
+        }
+    }
+}
