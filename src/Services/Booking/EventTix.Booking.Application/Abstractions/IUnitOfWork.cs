cc9ac984126918ddb46585d64884903bdad76e90
Andrea Parisi <andreaparisi917@gmail.com>
2026-08-03 15:11:14 +0200

    feature(booking): add UnitOfWork using DbContext

---
+++ src/Services/Booking/EventTix.Booking.Application/Abstractions/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿namespace EventTix.Booking.Application.Abstractions
+{
+    public interface IUnitOfWork
+    {
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
