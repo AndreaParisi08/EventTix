cf13d5c6e2c56813e8914ee6297c541274a7c931
Andrea Parisi <andreaparisi917@gmail.com>
2026-08-04 18:11:05 +0200

    feat(booking): first migration

---
+++ src/Services/Booking/EventTix.Booking.Infrastructure/Migrations/20260804154532_InitialCreate.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EventTix.Booking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "bookings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    seat_id = table.Column<string>(type: "text", nullable: false),
+                    price_amount = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    price_currency = table.Column<string>(type: "character varying(3)", maxLength: 3, nullable: false),
+                    status = table.Column<string>(type: "text", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    expires_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_bookings", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_bookings_seat_id_status",
+                table: "bookings",
+                columns: new[] { "seat_id", "status" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "bookings");
+        }
+    }
+}
